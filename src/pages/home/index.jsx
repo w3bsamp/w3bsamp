b8573f6147ecb04a1b94f8303319eb3066854806
Raphael Trindade <76216530+w3bsamp@users.noreply.github.com>
2024-10-08 14:46:37 -0300

    at

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './style.css'
+
+function Home() {
+  return (
+    <header>
+      <menu>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Skills</a></li>
+          <li><a href="#">Sobre-mim</a></li>
+          <li><a href="#">Projetos</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </menu>
+    </header>
+  )
+}
+
+export default Home
